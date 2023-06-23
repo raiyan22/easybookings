e2e56ae671dbb13312ae47f89e9cebff04903fde
raiyan22 <raiyanislam058@gmail.com>
2023-06-23 20:11:41 +0600

    first req res

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
-// raiyanislam058
-// xjx4lWc3AgvvNqVd
 
-app.listen(8800, ()=>{
+// important connection
+const connect = async () => { // does not retry
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        throw err;
+    }
+}
+
+mongoose.connection.on("disconnected", ()=>{ // retries
+    console.log("MongoDB disconnected!");
+})
+
+mongoose.connection.on("connected", ()=>{ // retries
+    console.log("MongoDB connected!");
+})
+
+
+app.get('/', (req, res) => {
+    res.send("hello");
+} )
+
+app.listen(8800, () => {
     console.log("Connected to backend...!");
 })
