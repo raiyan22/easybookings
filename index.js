71053704456a0a8818398e6567b01a1e7e179676
raiyan22 <raiyanislam058@gmail.com>
2023-06-23 19:21:32 +0600

    starting

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+
+const app = express();
+dotenv.config();
+// raiyanislam058
+// xjx4lWc3AgvvNqVd
+
+app.listen(8800, ()=>{
+    console.log("Connected to backend...!");
+})
