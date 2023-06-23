e12f69557fbc5c5731a2a71dc3c1536e50ba2f5e
raiyan22 <raiyanislam058@gmail.com>
2023-06-23 20:48:18 +0600

    before Test: Creating Hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,10 +1,21 @@
 import express from "express";
-import mongoose from "mongoose";
+import Hotel from '../models/Hotel.js'
+
 
 const router = express.Router();
 
 // Create
+router.post('/', async (req,res)=> {
 
+    const newHotel = new Hotel(req.body)
+
+    try {
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
 // Update
 // Delete
 // Get
