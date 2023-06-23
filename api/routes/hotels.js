a8c178a58bb52f7a239092bad35007aa881fe873
raiyan22 <raiyanislam058@gmail.com>
2023-06-23 21:32:16 +0600

    Updated hotel name

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -16,7 +16,18 @@ router.post("/", async (req,res)=> {
         res.status(500).json(err);
     }
 });
+
 // Update
+router.put("/:id", async (req,res)=> {
+
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body}, {new: true}); // new so that it shows updated item
+        res.status(200).json(updatedHotel);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 // Delete
 // Get
 // GetAll
